597748e51c09055c3b291c5a1ebf70a8609781ca
Lucas dias <lucasdiass061@gmail.com>
2022-10-10 21:32:01 -0300

    version 1.0

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const exphbs = require('express-handlebars');
 //inicializando o express, e usando a engine do Handlebars
 const app = express();
 
-app.engine('handlebars', exphbs.engine());
-app.set('view engine','handlebars');
+app.engine('handlebars', exphbs.engine())
+app.set('view engine','handlebars')
+//-----------------------------------------------------------------------------------------
+//chamando o css para estilizar a pagina
+app.use(express.static('public'))
 //-----------------------------------------------------------------------------------------
 //fazendo uma requizição para view aqui no index.js, chamando o home.handlebars, e fazendo a sua rota 
 app.get('/', (req,res) => {
-    res.resnder('home', {layout:false});
-});
+    res.render('home')//layout = false é para dizer, que não temos nenhum layout definido ainda
+})
 //-----------------------------------------------------------------------------------------
 //criando o listem, que serve para falar em qual porta esta rodando o servidor
 app.listen(3000, () =>{
-    console.log('app Funcionando');
-});
+    console.log('app Funcionando')
+})
