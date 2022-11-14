e10edbb5aad8373c75dd807e680b5dec7196b94e
Lucas dias <lucasdiass061@gmail.com>
2022-11-14 12:01:03 -0300

    version 1.3

--- index.js
+++ index.js
@@ -2,20 +2,86 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mysql = require('mysql');
+const session = require('express-session');
+const multer = require('multer');
 //-----------------------------------------------------------------------------------------
-//inicializando o express, e usando a engine do Handlebars
+//inicializando o express, Handlebar, mysql, express-session, multert
 const app = express();
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine','handlebars')
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//receber os campos do formulário---
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+app.use(express.json());
+//----------------------------------
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine','handlebars');
 //-----------------------------------------------------------------------------------------
 //chamando o css para estilizar a pagina
-app.use(express.static('public'))
+app.use(express.static('public'));
 //-----------------------------------------------------------------------------------------
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb) {
+
+        //Extração da extensão do arquivo original
+        const extensaoArquivo = file.originalname.split('.')[1];
+
+        //Cria um codigo randomico que será o nome do arquivo
+        const novoNomeArquivo = require('crypto')
+            .randomBytes(10)
+            .toString('hex');
+
+        //Indica o novo nome do arquivo:
+        cb(null ,`${novoNomeArquivo}.${extensaoArquivo}`)
+    }
+});
+
+const uploads = multer({storage});
+
+//-------------------------------------------------------------------------------------------
 //fazendo uma requizição para view aqui no index.js, chamando o home.handlebars, e fazendo a sua rota 
 app.get('/', (req,res) => {
-    res.render('home')//layout = false é para dizer, que não temos nenhum layout definido ainda
+    res.render('home');//layout = false é para dizer, que não temos nenhum layout definido ainda
 })
+
+app.get('/cadastro', (req,res) => {
+    res.render('cadastro');
+})
+
+//
+app.post('/cadastro', uploads.single('foto'), (req,res) => {
+
+    const usuario = req.body.usuario
+    const email = req.body.email
+    const senha = req.body.senha
+    //const caminho = req.file.destination
+    const arquivo = req.body.filename
+    const foto = arquivo
+    
+    const sql = `INSERT INTO tbl_usuario (usuario,email,senha,foto) VALUES ('${usuario}','${email}','${senha}','${foto}')`
+    conn.query(sql, function(err, dados) {
+        if (err) {
+            console.log(err)
+        }
+        res.redirect('/')
+    })
+})
+
 //-----------------------------------------------------------------------------------------
 //criando a conexão com o banco de dados 
 const conn = mysql.createConnection({
@@ -23,7 +89,7 @@ const conn = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'proOne'
-})
+});
 //------------------------------------------------------------------------------------------
 //verificando se conectou com o banco de dados e iniciando o listem 
 conn.connect(function (err){
@@ -32,6 +98,6 @@ conn.connect(function (err){
     }
     console.log('Conectou mysql!');
     app.listen(3000);
-})
+});
 
 
